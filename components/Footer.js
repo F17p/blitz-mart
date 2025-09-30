7c301501004cad66817670a53442f538304b7a04
F17p <felicianondala95@gmail.com>
2025-09-30 10:35:10 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+  return (
+    <footer className="mt-8 text-center text-sm text-slate-500">
+      <div>© Blitz Mart — Feito com cuidado</div>
+      <div className="mt-1">Contacto: contato@blitzmart.example • Redes: @blitzmart</div>
+    </footer>
+  );
+}
